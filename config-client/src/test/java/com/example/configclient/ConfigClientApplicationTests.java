9ade7973a6a9dfe8e71f132c3fda005ac2004c24
yangstars <951020602@qq.com>
2018-03-02 11:49:21 +0800

    配置文件

--- config-client/src/test/java/com/example/configclient/ConfigClientApplicationTests.java
+++ config-client/src/test/java/com/example/configclient/ConfigClientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.configserver;
+package com.example.configclient;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfigServerApplicationTests {
+public class ConfigClientApplicationTests {
 
 	@Test
 	public void contextLoads() {
